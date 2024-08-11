7c1818dc7ffd93064b7d1d374b4a80b41ef9aa13
shai <shai@bigdataboutique.com>
2024-08-11 14:35:32 +0300

    fixing templates issue and missing variables

--- terraform-aws/alb.tf
+++ terraform-aws/alb.tf
@@ -82,7 +82,7 @@ resource "aws_lb" "elasticsearch-alb" {
   internal           = ! var.public_facing
   load_balancer_type = "application"
   security_groups    = [aws_security_group.elasticsearch-alb-sg.id]
-  subnets            = coalescelist(var.cluster_subnets, tolist(data.aws_subnets.all-subnets.ids))
+  subnets            = coalescelist(var.cluster_subnet_ids, tolist(data.aws_subnets.all-subnets.ids))
 
   enable_deletion_protection = false
 }
