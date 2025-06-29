514d75949183623d899266b72e52e2168a644f1b
shai <shai@bigdataboutique.com>
2025-06-29 17:43:56 +0300

    safer nodecount syntax

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -34,11 +34,11 @@ locals {
 
   flat_clients_subnet_ids = flatten(values(local.clients_subnet_ids))
 
-  singlenode_mode = sum(try(values(var.masters_blue_count), [])) + sum(try(values(var.masters_count), [])) + sum(try(values(var.data_voters_count), [])) + sum(try(values(var.data_voters_blue_count), [])) +  sum(try(values(var.datas_count), [])) +  sum(try(values(var.datas_blue_count), [])) == 0
+  singlenode_mode = sum(try([for v in values(var.masters_blue_count) : v], [])) + sum(try([for v in values(var.masters_count) : v], [])) + sum(try([for v in values(var.data_voters_count) : v], [])) + sum(try([for v in values(var.data_voters_blue_count) : v], [])) +  sum(try([for v in values(var.datas_count) : v], [])) +  sum(try([for v in values(var.datas_blue_count) : v], [])) == 0
 
 
   alb_kibana_groups    = local.singlenode_mode ? toset(var.alb_security_groups) : toset([])
-  masters_count = local.singlenode_mode ? 0 : sum(try(values(var.masters_blue_count), [])) + sum(try(values(var.masters_count), [])) + sum(try(values(var.data_voters_count), [])) + sum(try(values(var.data_voters_blue_count), []))
+  masters_count = local.singlenode_mode ? 0 : sum(try([for v in values(var.masters_blue_count) : v], [])) + sum(try([for v in values(var.masters_count) : v], [])) + sum(try([for v in values(var.data_voters_count) : v], [])) + sum(try([for v in values(var.data_voters_blue_count) : v], []))
   
   is_cluster_bootstrapped = !var.requires_bootstrapping
 
